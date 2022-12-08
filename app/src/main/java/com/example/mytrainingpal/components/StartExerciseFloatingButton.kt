450150bdd7f9703fd6d6273a3d4c08d64ad68f8d
lenilol <emil.hillebrand@gmx.de>
2022-12-08 16:08:51 +0100

    change color of dumbell icon in floating button

--- app/src/main/java/com/example/mytrainingpal/components/StartExerciseFloatingButton.kt
+++ app/src/main/java/com/example/mytrainingpal/components/StartExerciseFloatingButton.kt
@@ -2,13 +2,15 @@ package com.example.mytrainingpal.components
 
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FitnessCenter
 import androidx.compose.material.icons.filled.Sports
 import androidx.compose.runtime.Composable
 
 @Composable
 fun StartExerciseFloatingButton(navigateToTrainingScreen: () -> Unit) {
     FloatingActionButton(onClick = { navigateToTrainingScreen() }) {
-        Icon(imageVector = Icons.Default.Sports, contentDescription = "Start Training")
+        Icon(imageVector = Icons.Default.FitnessCenter, tint = MaterialTheme.colors.onSecondary, contentDescription = "Start Training")
     }
 }
