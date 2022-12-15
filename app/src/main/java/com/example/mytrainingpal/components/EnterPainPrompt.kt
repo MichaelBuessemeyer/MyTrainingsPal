dad4978886689d2dd8efc887482711a35f6e05b1
raissacavalcanti <89835939+raissacavalcanti@users.noreply.github.com>
2022-12-15 20:46:35 +0100

    add Card prompting user to enter their Muscle Pain - component receives navigation link as parameter

---
+++ app/src/main/java/com/example/mytrainingpal/components/EnterPainPrompt.kt
@@ -0,0 +1,57 @@
+package com.example.mytrainingpal.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EnterPainPrompt(navigateToMusclePain: () -> Unit) {
+
+// Card Reference: https://foso.github.io/Jetpack-Compose-Playground/material/card/
+    Card(
+        border = BorderStroke(2.dp, MaterialTheme.colors.secondary),
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        elevation = 10.dp,
+
+    ){
+        Column(
+            //modifier = Modifier.padding(20.dp)
+        ){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "Please Enter Your Sore Muscles",
+                    color = MaterialTheme.colors.secondary,
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    // weight otherwise Icon is not displayed
+                    modifier = Modifier.weight(1f),
+                )
+                IconButton(
+                    onClick = { navigateToMusclePain() }
+                ){
+                    Icon(
+                        Icons.Default.Edit, contentDescription = "Edit Muscle Pain",
+                        tint =  MaterialTheme.colors.secondary
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/mytrainingpal/screens/Home.kt
+++ app/src/main/java/com/example/mytrainingpal/screens/Home.kt
@@ -3,7 +3,7 @@ package com.example.mytrainingpal.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -26,11 +26,18 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun HomeScreenContent(navigateToMusclePain: () -> Unit = {}, navigateToSettings: () -> Unit = {}) {
+
+    var wasPainEntered by remember { mutableStateOf(true) }
+    //TODO wasPainEntered should be updated in the model, toggled to true or false
+    // in that case we need a Model instance passed into HomeScreenContent
     Column(
         Modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         UserNameWithSettings(navigateToSettings)
+        if(wasPainEntered){
+            EnterPainPrompt(navigateToMusclePain = navigateToMusclePain)
+        }
         MusclePainWidget(navigateToMusclePain = navigateToMusclePain)
         OverallRecordsCard()
         OverallRecordsCard()
