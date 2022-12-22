647eac3e8d242f561f868f8482a7ee9bce78a613
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2022-12-22 23:55:06 +0100

    Add all exercises we have on first db creating of the app

--- app/src/main/java/com/example/mytrainingpal/model/Muscle.kt
+++ app/src/main/java/com/example/mytrainingpal/model/Muscle.kt
@@ -5,7 +5,7 @@ import androidx.room.*
 // To create the modal package, we followed the tutorial posted by MikeT at
 // https://stackoverflow.com/questions/68682797/could-someone-help-me-understand-room-persistence-library-better
 // accessed on 4th Dec 2022.
-@Entity()
+@Entity(indices = [Index("muscleId"), Index("name")])
 data class Muscle(
     @PrimaryKey
     val muscleId: Long? = null,
