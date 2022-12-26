f51e699973a1c798a7f9c3fd9de21aa57d92b084
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2022-12-26 21:39:02 +0100

    fix merge

--- app/src/main/java/com/example/mytrainingpal/screens/Home.kt
+++ app/src/main/java/com/example/mytrainingpal/screens/Home.kt
@@ -3,10 +3,7 @@ package com.example.mytrainingpal.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -50,7 +47,7 @@ fun HomeScreenContent(
     musclePainEntryMapViewModel: MusclePainEntryMapViewModel,
 ) {
     val todaysMusclePainEntry = RememberTodaysMusclePainEntryState(musclePainEntryViewModel)
-    val exercise: Exercise = Exercise(name = "Test", pathToGif = "somePath")
+    val exercise = Exercise(name = "Test", pathToGif = "somePath")
     val weight by remember { mutableStateOf(20) }
     var sets by remember { mutableStateOf(4) }
     var reps by remember { mutableStateOf(10) }
