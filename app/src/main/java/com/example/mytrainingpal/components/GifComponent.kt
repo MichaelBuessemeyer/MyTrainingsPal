a0335dc5cc3e567e0dc84ec23c3d9d83229848a1
raissacavalcanti <89835939+raissacavalcanti@users.noreply.github.com>
2022-12-26 22:09:00 +0100

    Separate composable that will render a gif where called (#25)
    * Separate composable that will render a gif where called. Takes an int for the gif size and a gif "path" (format: R.drawable.<gifName>)
    
    * GifImage composable now takes a string path in the format "exercise_12"
    
    * fix Exercise widget
    
    Co-authored-by: Michael Büßemeyer <michael.buessemeyer@student.hpi.de>

---
+++ app/src/main/java/com/example/mytrainingpal/components/GifComponent.kt
@@ -0,0 +1,54 @@
+package com.example.mytrainingpal.components
+
+import android.os.Build
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+
+// Reference: Dr BigKitkat answer on github + Coil library docs
+// https://stackoverflow.com/questions/60229555/adding-gif-into-jetpack-compose
+// about AsyncImagePainter --> https://coil-kt.github.io/coil/compose/
+
+
+@Composable
+fun GifImage(
+    gifPath: String,
+    size: Int
+) {
+
+    val context = LocalContext.current
+    // Getting the resource id of the gif using its path.
+    // Code from https://stackoverflow.com/questions/70062705/how-can-i-get-drawable-resource-by-string was used.
+    val gifResourceId = remember(gifPath) {
+        context.resources.getIdentifier(
+            gifPath,
+            "drawable",
+            context.packageName
+        )
+    }
+
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            if (Build.VERSION.SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
+
+    Image(
+        painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(context).data(data = gifResourceId).apply(block = {
+                size(size)
+            }).build(), imageLoader = imageLoader
+        ),
+        contentDescription = null,
+    )
+}
