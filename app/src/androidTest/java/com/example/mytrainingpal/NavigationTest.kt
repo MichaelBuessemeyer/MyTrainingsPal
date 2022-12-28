3717470be51c06f102712fd2811d13630edd03eb
grittaweisheit <grittaweisheit@gmail.com>
2022-12-28 21:15:58 +0100

    add a few very basic nav test

--- app/src/androidTest/java/com/example/mytrainingpal/NavigationTest.kt
+++ app/src/androidTest/java/com/example/mytrainingpal/NavigationTest.kt
@@ -1,12 +1,13 @@
 package com.example.mytrainingpal
 
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.example.mytrainingpal.components.AppNavHost
+import com.example.mytrainingpal.components.Screen
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -28,9 +29,38 @@ class NavigationTest {
     // Unit test
     @Test
     fun appNavHost_verifyStartDestination() {
-        composeTestRule
-            .onNodeWithContentDescription("Start Screen")
-            .assertIsDisplayed()
+        composeTestRule.runOnIdle {
+            assertEquals(navController.currentDestination?.route, Screen.Home.route)
+        }
+    }
+
+    @Test
+    fun bottomNavBar_verifyStartTabHomeSelected() {
+        composeTestRule.onNodeWithText("Home").assertIsSelected()
+    }
+
+    @Test
+    fun bottomNavBar_verifyStartTabOnlyHomeSelected() {
+        composeTestRule.onRoot().printToLog("onRoot")
+        composeTestRule.onNodeWithText("Calendar").assertIsNotSelected()
+        composeTestRule.onNodeWithText("Muscle Pain").assertIsNotSelected()
+        composeTestRule.onNodeWithText("Training").assertIsNotSelected()
+    }
+
+    @Test
+    fun bottomNavBar_verifySNavToCalendar() {
+        composeTestRule.onNodeWithText("Calendar").performClick()
+        composeTestRule.runOnIdle {
+            assertEquals(navController.currentDestination?.route, Screen.CalendarMain.route)
+        }
+    }
+
+    @Test
+    fun homeFloatingButton_verifyNavToTraining() {
+        composeTestRule.onNodeWithContentDescription("Start Training").performClick()
+        composeTestRule.runOnIdle {
+            assertEquals(navController.currentDestination?.route, Screen.TrainingMain.route)
+        }
     }
 }
 
